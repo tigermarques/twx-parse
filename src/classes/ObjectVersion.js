56fcc832afab50ba8357ebdeb28176edfedefa65
Joao Marques <jgmarques@kpmg.com>
2019-10-29 15:28:26 +0000

    fix: fix ObjectVersion to correctly build objects for DB handler

--- src/classes/ObjectVersion.js
+++ src/classes/ObjectVersion.js
@@ -5,6 +5,17 @@ const getObjectFromResult = workspaceName => item =>
     ? new ObjectVersion(workspaceName, item.objectVersionId, item.objectId, item.name, item.type, item.subtype)
     : null
 
+const getObjectFromItem = obj =>
+  obj
+    ? {
+      objectVersionId: obj.objectVersionId,
+      objectId: obj.objectId,
+      name: obj.name,
+      type: obj.type,
+      subtype: obj.subtype
+    }
+    : null
+
 class ObjectVersion {
   constructor (workspaceName, objectVersionId, objectId, name, type, subtype) {
     this.workspace = workspaceName
@@ -19,8 +30,8 @@ class ObjectVersion {
   }
 }
 
-ObjectVersion.register = (workspaceName, item) => DBAccess.register(workspaceName, item)
-ObjectVersion.registerMany = (workspaceName, items) => DBAccess.registerMany(workspaceName, items)
+ObjectVersion.register = (workspaceName, item) => DBAccess.register(workspaceName, getObjectFromItem(item))
+ObjectVersion.registerMany = (workspaceName, items) => DBAccess.registerMany(workspaceName, items.map(getObjectFromItem))
 ObjectVersion.getAll = (workspaceName) => DBAccess.getAll(workspaceName).then(results => results.map(getObjectFromResult(workspaceName)))
 ObjectVersion.getById = (workspaceName, objectVersionId) => DBAccess.getById(workspaceName, objectVersionId).then(result => getObjectFromResult(workspaceName)(result))
 ObjectVersion.where = (workspaceName, obj, snapshotObj) => DBAccess.where(workspaceName, obj, snapshotObj).then(results => results.map(getObjectFromResult(workspaceName)))
